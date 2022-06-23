bd6496dede49b6571b05847bda69cea170f7bf48
Matthias Geisler <matthias.geisler@data4life.care>
2022-06-23 20:34:58 +0200

    Move Package.json

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod util;
 
+use num_bigint_dig::RandPrime;
+use rand::prelude::*;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +11,10 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+#[allow(non_snake_case)]
+pub fn probablyPrime(size: usize) -> String {
+    let mut random = rand::thread_rng();
+    let prime = random.gen_prime(size);
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, {{project-name}}!");
+    prime.to_str_radix(10)
 }
